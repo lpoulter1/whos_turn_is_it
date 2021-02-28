b1ed820f50d6e615c940e706734f3744ae126ccf
laurie <laurie@brandwatch.com>
2021-02-28 18:09:18 +0000

    Post message to slack channel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-const axios = require('axios').default;
+require('dotenv').config()
+const axios = require("axios").default;
 const cheerio = require("cheerio");
 
 const gameId = `3851196`;
 const gameLink = `http://www.boiteajeux.net/jeux/agr/partie.php?id=${gameId}`;
 
-axios.get(gameLink)
-  .then(function (response) {
+const lastPlayer = '';
+const playerIdMap = { 'lpoulter': 'U01PMMBSQSF' }
+
+https: axios
+  .get(gameLink)
+  .then(function(response) {
     const $ = cheerio.load(response.data);
-    const nextPlayer = $('.clInfo').text();
-    console.log(nextPlayer);
+    const nextPlayer = $(".clInfo").text();
+
+    if(nextPlayer !== lastPlayer) {
+        axios.post(
+          process.env.AGRICOLA_NOTIFICATION_CHANNEL_WEB_HOOK,       
+            { text: `${nextPlayer} <@U01PMMBSQSF>` }
+          );
+    }
   })
-  .catch(function (error) {
+  .catch(function(error) {
     // handle error
     console.log(error);
   })
-  .then(function () {
+  .then(function() {
     // always executed
   });

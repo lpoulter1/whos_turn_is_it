f4ba55d83ef54ec5588be2a5db801d968b6b411c
laurie <laurie@brandwatch.com>
2021-04-11 09:37:44 +0100

    send slack notifcation.

--- src/scrapeBga.js
+++ src/scrapeBga.js
@@ -1,5 +1,7 @@
-const puppeteer = require("puppeteer");
+require("dotenv").config();
 const fs = require("fs");
+const puppeteer = require("puppeteer");
+const axios = require("axios").default;
 
 function writeLastPlayedToDisk(fileName, lastPlayed = "") {
   try {
@@ -10,20 +12,42 @@ function writeLastPlayedToDisk(fileName, lastPlayed = "") {
   }
 }
 
+function getLastPlayed({ tableId, fileName }) {
+  let lastPlayedData;
+  let lastPlayed = "";
+
+  try {
+    lastPlayedData = fs.readFileSync(fileName);
+    lastPlayed = JSON.parse(lastPlayedData).lastPlayed;
+  } catch (error) {
+    console.log(`Creating JSON file for game ${tableId} `);
+    writeLastPlayedToDisk(fileName, "");
+  }
+
+  return lastPlayed;
+}
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const tableId = "162693887";
+  const fileName = `last-played-terra-mystica-${tableId}.json`;
   const gameUrl = `https://boardgamearena.com/9/terramystica?table=${tableId}`;
   await page.goto(gameUrl);
   await page.waitForSelector("#pagemaintitletext");
 
-  const turnString = await page.evaluate(() => {
+  const lastPlayed = getLastPlayed({ tableId, fileName });
+  const nextPlayer = await page.evaluate(() => {
     const turnString = document.querySelector("#pagemaintitletext").textContent;
     return turnString;
   });
 
-  writeLastPlayedToDisk(`${tableId}.json`, turnString);
+  if (nextPlayer !== lastPlayed) {
+    writeLastPlayedToDisk(fileName, nextPlayer);
+    axios.post(process.env.TEST_CHANNEL_WEB_HOOK, {
+      text: `${nextPlayer} \n ${gameUrl}`,
+    });
+  }
 
   await browser.close();
 })();
